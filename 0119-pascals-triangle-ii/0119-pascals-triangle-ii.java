f68b68c75681b0252582b058d99ebe527718e433
jananiselvaraj26 <88244041+jananiselvaraj26@users.noreply.github.com>
2022-10-12 19:18:31 -0500

    Time: 2 ms (64.02%), Space: 41.6 MB (60.45%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,29 +1,16 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-       List<List<Integer>> result=new ArrayList<List<Integer>>();
-        for(int i=0;i<=rowIndex;i++){
-            int leftIndex=0;
-            int rightIndex=i;
-            List<Integer> curr=new ArrayList<Integer>();
-            for(int j=0;j<=i;j++){
-                if(j==leftIndex || j==rightIndex){
-                    curr.add(1);
-                }
-                else{
-                    
-                    int prev=result.get(i-1).get(j-1);
-                    int next=result.get(i-1).get(j);
-                    
-                    curr.add(prev+next);
-                    
-                    
-                }
+        List<Integer> res=new ArrayList<Integer>();
+        int[] ans=new int[rowIndex+1];
+        ans[0]=1;
+        for(int i=1;i<=rowIndex;i++){
+            for(int j=i;j>0;j--){
+                ans[j]=ans[j]+ans[j-1];
             }
-            result.add(curr);
         }
-        
-        
-        
-        return result.get(rowIndex);
+        for(int i:ans){
+            res.add(i);
+        }
+      return res;
     }
 }
