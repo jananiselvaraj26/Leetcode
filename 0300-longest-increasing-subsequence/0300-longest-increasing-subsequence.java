c49d4e44598453936a4b9b655784b242a6fcbea4
jananiselvaraj26 <88244041+jananiselvaraj26@users.noreply.github.com>
2022-10-16 12:10:07 -0500

    Time: 5 ms (93.11%), Space: 44.7 MB (29.70%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,6 +1,7 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] lis=new int[nums.length];
+    /** approach using DP
+    int[] lis=new int[nums.length];
         lis[0]=1;
         for(int i=1;i<nums.length;i++){
             lis[i]=1;
@@ -16,6 +17,41 @@ class Solution {
             res=Math.max(res,lis[i]);
         }
         return res;
-        
+     *
+     */
+        /**Approach using binary search*/
+       int[] tail=new int[nums.length];
+        int res=1;
+        tail[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>tail[res-1]){
+                tail[res]=nums[i];
+                res++;
+            }
+            else{
+                int index=ceilIndex(tail,0,res-1,nums[i]);
+                tail[index]=nums[i];
+            }
+        }
+    return res;    
     }
+    
+    public int ceilIndex(int[] tail,int l,int r,int x){
+        
+        while(r>l){
+            int m=l+(r-l)/2;
+            if(tail[m]>=x){
+              r=m;
+            }
+            else{
+                l=m+1;
+            }
+            
+            
+        }
+        return r;
+    }
+    
+    
+    
 }
