479fd4f2d3d64e2b4e5a29a35b3f0aec943712af
jananiselvaraj26 <88244041+jananiselvaraj26@users.noreply.github.com>
2022-10-16 17:39:19 -0500

    Time: 9 ms (42.58%), Space: 65.7 MB (12.04%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,5 +1,12 @@
 class Solution {
     public int maxAbsoluteSum(int[] nums) {
+        
+        /*
+       max prefix sum = max(nums[0] + ... + nums[i - 1]), where 0 <= i <= n;
+min prefix sum = min(nums[0] + ... + nums[j - 1]), where 0 <= j <= n;
+The above will be 0, if i == 0 and j == 0, respectively.
+
+max abs of subarray sum = max prefix sum - min prefix sum */
         int prefixSum=0;
         int max=0;
         int min=0;
