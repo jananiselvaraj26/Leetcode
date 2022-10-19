b048ea986b99d4509d9039880c6cded01306d348
jananiselvaraj26 <88244041+jananiselvaraj26@users.noreply.github.com>
2022-10-19 17:55:23 -0500

    Time: 12 ms (29.63%), Space: 63.4 MB (75.14%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    /**
+    1)first find Max subarray sum uing kadane algorithm
+    2)find max circular sum array by inverting the values in array and then find max subarray using same kadane(find min of submarray for circular array)
+    3) get max from both normalsum and circular sum.
+    
+    */
+    public int maxSubarraySumCircular(int[] nums) {
+        
+        int res=0;
+        int normalMax=kadane(nums);
+        if(normalMax<0){
+            return normalMax;
+        }
+        else{
+            int arrSum=0;
+            
+            for(int i=0;i<nums.length;i++){
+                arrSum+=nums[i];
+                nums[i]=-nums[i];
+            }
+            int circularSum=arrSum+(kadane(nums));
+            
+            
+            return Math.max(circularSum,normalMax);
+        }
+        
+    }
+    
+    public int kadane(int[] nums){
+        
+        int res=nums[0];
+        int max=nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+           max=Math.max(nums[i],(nums[i]+max));
+            
+            res=Math.max(max,res);
+        }
+        
+        return res;
+    }
+}
